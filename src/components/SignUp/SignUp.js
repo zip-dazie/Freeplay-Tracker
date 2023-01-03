0544b74b14d9848c59caa7a975177dcad2950b95
kylerpan <kylerpan691@gmail.com>
2023-01-02 23:41:00 -0800

    created error messages and fixed horizontal scroll

--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -1,21 +1,49 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './SignUp.css';
-import { addStudent } from '../Users/Users';
+import { addStudent, checkNumber } from '../Users/Users';
 
 function SignUp() {
   const [phoneNumber, setPhoneNumber] = useState('');
   const [name, setName] = useState('');
+  const [phoneError, setPhoneError] = useState(false);
+  const [phoneEmptyError, setPhoneEmptyError] = useState(false);
+  const [nameEmptyError, setNameEmptyError] = useState(false);
 
-  const handleSubmit = () => {
-    alert('sign up form was submitted');
+  useEffect(() => {
+    if (name != '') setNameEmptyError(false);
+  }, [name]);
+
+  useEffect(() => {
+    if (phoneNumber != '') setPhoneEmptyError(false);
+  }, [phoneNumber]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (phoneError) return;
+
+    if (phoneNumber === '' || phoneNumber.length != 10) {
+      setPhoneEmptyError(true);
+      return;
+    }
+
+    if (name === '') {
+      setNameEmptyError(true);
+      return;
+    }
+
     addStudent(phoneNumber, name);
+    window.location.href = '/';
   };
 
-  const checkPhoneNumber = (e) => {
+  const updatePhoneNumber = (e) => {
     if (!e.target.value.match('^[0-9]*$') || e.target.value.length > 10) {
       e.target.value = e.target.value.slice(0, -1);
     }
+
     setPhoneNumber(e.target.value);
+
+    if (e.target.value.length == 10 && checkNumber(e.target.value)) setPhoneError(true);
+    else setPhoneError(false);
   };
 
   return (
@@ -26,19 +54,33 @@ function SignUp() {
           <button>Sign up &nbsp;&rarr;</button>
         </div>
         <label className="label">Phone Number</label>
-        <input
-          type="text"
-          value={phoneNumber}
-          placeholder="What is your phone number? e.g. 7463785928"
-          onChange={(e) => checkPhoneNumber(e)}
-        />
+        <div>
+          <input
+            type="text"
+            value={phoneNumber}
+            className={phoneError || phoneEmptyError ? 'input error' : 'input'}
+            placeholder="What is your phone number? e.g. 7463785928"
+            onChange={(e) => updatePhoneNumber(e)}
+          />
+          {phoneError && <p className="error-message">This phone number has been taken!</p>}
+          {phoneEmptyError && phoneNumber === '' && (
+            <p className="error-message">This field is required!</p>
+          )}
+          {phoneEmptyError && phoneNumber != '' && (
+            <p className="error-message">Phone number must be 10 digits long!</p>
+          )}
+        </div>
         <label className="label">Name</label>
-        <input
-          type="text"
-          value={name}
-          placeholder="What is your name?"
-          onChange={(e) => setName(e.target.value)}
-        />
+        <div>
+          <input
+            type="text"
+            value={name}
+            className={nameEmptyError ? 'input error' : 'input'}
+            placeholder="What is your first and last name?"
+            onChange={(e) => setName(e.target.value)}
+          />
+          {nameEmptyError && <p className="error-message">This field is required!</p>}
+        </div>
       </form>
     </div>
   );

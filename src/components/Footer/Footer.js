7456ca85dfa45123573e7d31d610bda99d199449
Ethan Chen <weic1709@gmail.com>
2023-05-07 18:38:13 -0700

    fixed withdraw bug

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,7 +5,7 @@ function Footer() {
     <div className="Footer">
       <div className="footer-left">
         <p className="footer-text">Made with 💙 by zip-dazie</p>
-        <p className="footer-text">2023 © Badminton Club at UCI — Version 1.2.0</p>
+        <p className="footer-text">2023 © Badminton Club at UCI — Version 1.2.1</p>
       </div>
       <div className="footer-right">
         <a

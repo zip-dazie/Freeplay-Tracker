6c3344adb5efa7c95314778c83a128be6531aa78
Ethan Chen <weic1709@gmail.com>
2023-05-17 03:18:32 -0700

    fixed player.id bug --> using index)

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,7 +5,7 @@ function Footer() {
     <div className="Footer">
       <div className="footer-left">
         <p className="footer-text">Made with 💙 by zip-dazie</p>
-        <p className="footer-text">2023 © Badminton Club at UCI — Version 1.3.0</p>
+        <p className="footer-text">2023 © Badminton Club at UCI — Version 1.3.1</p>
       </div>
       <div className="footer-right">
         <a
